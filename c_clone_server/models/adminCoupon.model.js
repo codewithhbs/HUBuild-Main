8dacf4a95e8eaca2c0f59caf6011a67cced49684
anish-stack <anishjha896@gmail.com>
2025-08-11 12:43:50 +0530

    add

--- c_clone_server/models/adminCoupon.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const AdminCouponSchema = new mongoose.Schema({
-    couponCode: { type: String, required: true },
-    discount: { type: Number, required: true, default:0 },
-})
-
-const AdminCoupon = mongoose.model('AdminCoupon',AdminCouponSchema)
-module.exports = AdminCoupon;
--- c_clone_server/models/globelUserRefDis.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const globelUserRefDisSchema = new mongoose.Schema({
-    discountPercent: { type: Number, default: 10 }
-}, { timestamps: true })
-
-const GlobelUserRefDis = mongoose.model('GlobelUserRefDis', globelUserRefDisSchema)
-module.exports = GlobelUserRefDis
